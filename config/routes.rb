b5ffc5607b17cab80ccedcbc49de0b32a045ad45
martylanger <langer.marty@gmail.com>
2020-03-31 12:26:35 -0400

    Scaffold and migrate choices
    Generate and scaffold
      bin/rails generate scaffold choice title:string description:string
      link:string
    Add to title
      null: false
    Migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :choices
   # RESTful routes
   resources :elections, except: %i[new edit]
   resources :ballots, except: %i[new edit]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_161941) do
+ActiveRecord::Schema.define(version: 2020_03_31_162534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_03_31_161941) do
     t.datetime "updated_at", null: false
     t.bigint "election_id"
     t.index ["election_id"], name: "index_ballots_on_election_id"
+  end
+
+  create_table "choices", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description"
+    t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "elections", force: :cascade do |t|
