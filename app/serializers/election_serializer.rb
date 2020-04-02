1629c777eb19ba1436367cc57cec5e4e9b5d0c4c
martylanger <langer.marty@gmail.com>
2020-04-02 18:07:30 -0400

    Add ballots and choices to election serializer

--- app/serializers/election_serializer.rb
+++ app/serializers/election_serializer.rb
@@ -1,4 +1,6 @@
 class ElectionSerializer < ActiveModel::Serializer
   attributes :id, :is_profile, :has_previews, :noms, :voter_opts, :privacy_opts, :close_time, :group, :name, :description, :voting_method, :result
+  has_many :choices
+  has_many :ballots
   belongs_to :user
 end
